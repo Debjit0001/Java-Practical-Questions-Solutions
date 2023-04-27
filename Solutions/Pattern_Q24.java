f989f938ed6c6dd04da95b76a4a09241cb5b2962
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-04-27 20:45:17 +0530

    Pattern program Q.24 solved

--- Solutions/Pattern_Q24.java
+++ Solutions/Pattern_Q24.java
@@ -1,4 +1,4 @@
-public class Pattern_Example {
+public class Pattern_Q24 {
     public static void main(String[] args) {
         int n = 4; // the number of rows can also be inputted
         int num = 1;
