47b71b2b4e80f4ba0f2273685eb2038623cf1504
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-04-27 14:47:38 +0530

    Update PatternExample.java

--- Solutions/PatternExample.java
+++ Solutions/PatternExample.java
@@ -1,20 +1,13 @@
 public class PatternExample {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 4; // the number of rows can also be inputted
         int num = 1;
-
        
-        for (int i = 1; i <= n; i++) {
-           
-            for (int j = 1; j <= i; j++) {
-                
+        for (int i = 1; i <= n; i++) {           
+            for (int j = 1; j <= i; j++) {                
                 System.out.print(num + " ");
-
-              
                 num += 2;
-            }
-
-            
+            }        
             System.out.println();
         }
     }
