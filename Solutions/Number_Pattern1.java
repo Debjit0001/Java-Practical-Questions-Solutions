7daaf5c0a9edee2c0753d883516d4c02b8187533
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-04-27 14:12:29 +0530

    Update Number_Pattern1.java

--- Solutions/Number_Pattern1.java
+++ Solutions/Number_Pattern1.java
@@ -1,22 +1,13 @@
-package JAVA_PROGRAM_CLG;
-
 public class PatternExample {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 4; // the total number of rows can also be inputted
         int num = 1;
 
-       
         for (int i = 1; i <= n; i++) {
-           
             for (int j = 1; j <= i; j++) {
-                
                 System.out.print(num + " ");
-
-              
                 num += 2;
             }
-
-            
             System.out.println();
         }
     }
