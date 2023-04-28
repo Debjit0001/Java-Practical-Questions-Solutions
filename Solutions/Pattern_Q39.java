7393c5a7d44d2818c2263ecf332ce0abe55c21d3
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-04-28 07:59:54 +0530

    Pattern Ques.39 solved

--- Solutions/Pattern_Q39.java
+++ Solutions/Pattern_Q39.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Pattern {
+public class Pattern_Q39 
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
