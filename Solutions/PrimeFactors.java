611b7e8062f41a58b69c8ab41533af5de820bc15
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-04-28 20:45:22 +0530

    Updated PrimeFactors.java

--- Solutions/PrimeFactors.java
+++ Solutions/PrimeFactors.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
-public class PrimeFactors {
+public class PrimeFactors 
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
         scanner.close();
-        System.out.print("Prime factors of " + num + " are: ");
-        for (int i = 2; i <= num; i++) {
+        
+        System.out.println("Prime factors of " + num + " are: ");
+        for (int i = 2; i <= num; i++) 
+        {
             while (num % i == 0) {
                 System.out.print(i + " ");
                 num /= i;
