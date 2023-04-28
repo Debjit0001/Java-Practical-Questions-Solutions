e5cdb7b28668b28e6dde6ca9f4f91175dc162c75
Arnab Pratihar <apratihar18@gmail.com>
2023-04-28 20:15:18 +0530

    DiagonalSum of a matrix #Q:43 solved

---
+++ Solutions/DiagonalSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class DiagonalSum{
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter number the length of the matrix:");
+        int n=ob.nextInt();
+        int[][]arr=new int[n][n];
+        System.out.println("Enter elements of the matrix:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j]=ob.nextInt();
+            }
+        }
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            int j=arr.length-1-i;
+         if(i==j)
+           sum+=arr[i][j];
+        else
+        sum+=arr[i][i]+arr[i][j];
+        }
+        System.out.println("The diagonal sum of this matrix is:"+sum);
+        ob.close();
+    }
+}
