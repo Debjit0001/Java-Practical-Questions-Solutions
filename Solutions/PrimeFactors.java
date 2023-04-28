8fd8fc4633bb92edfcbcdec060f5d28afdcf5a37
Atul <royatul031@gmail.com>
2023-04-28 19:20:54 +0530

    Prime Factors of a given number solved

---
+++ Solutions/PrimeFactors.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        System.out.print("Prime factors of " + num + " are: ");
+        for (int i = 2; i <= num; i++) {
+            while (num % i == 0) {
+                System.out.print(i + " ");
+                num /= i;
+            }
+        }
+    }
+}
+
