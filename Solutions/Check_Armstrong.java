8c389cc258f6e9625ae0e1ada4306df55afb1132
Debjit <debjitm234@gmail.com>
2023-04-28 07:56:09 +0530

    Added power of a number

--- Solutions/Check_Armstrong.java
+++ Solutions/Check_Armstrong.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-class Main {
+class Check_Armstrong {
     public static void main(String[] args) {
         Scanner ob = new Scanner(System.in);
         System.out.println("Enter an integer number:");
         int n = ob.nextInt();
-        if (checkArm(n))
+        if (isArmstrong(n))
             System.out.println("The number is an Armstrong number");
         else
             System.out.println("The number is not an Armstrong number");
         ob.close();
     }
 
-    static boolean checkArm(int n) {
+    static boolean isArmstrong(int n) {
         int c = (int) (Math.log10(n) + 1);
         int p = n;
         int s = 0;
