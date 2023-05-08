81eb63009d3b8bf5c25617435edc832bd877d77b
Debjit <debjitm234@gmail.com>
2023-05-08 12:20:00 +0530

    Updated

--- Solutions/ConstructorOverload.java
+++ Solutions/ConstructorOverload.java
@@ -1,21 +1,23 @@
-class Over {
-    public Over(int a, int b) {
-        System.out.println("Here both data are integer");
-    }
-
-    public Over(int a, float b) {
-        System.out.println("Here one data is integer and another is float");
-    }
-
-    public Over(int a, double b) {
-        System.out.println("Here one data is integer and another is double");
+public class ConstructorOverload 
+{
+    public static void main(String[] args) {
+        new Overloaing(10, 12);
+        new Overloaing(10, 12.0f);
+        new Overloaing(10, 12.00);
     }
 }
 
-public class ConstructorOverload {
-    public static void main(String[] args) {
-        Over ob1 = new Over(10, 12);
-        Over ob2 = new Over(10, 12.0f);
-        Over ob3 = new Over(10, 12.00);
+class Overloaing
+{
+    public Overloaing(int a, int b) {
+        System.out.println("Here both parameters are integer");
+    }
+
+    public Overloaing(int a, float b) {
+        System.out.println("Here one parameter is integer and another is float");
+    }
+
+    public Overloaing(int a, double b) {
+        System.out.println("Here one parameter is integer and another is double");
     }
 }
